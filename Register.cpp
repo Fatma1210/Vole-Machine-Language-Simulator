febfda39674ebfa6e05626996d0070511b4d4614
Fatma İbrahim <113267262+Fatma1210@users.noreply.github.com>
2023-11-12 12:36:58 +0200

    Update Register.cpp

--- Register.cpp
+++ Register.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 using namespace std ;
 #include "Register.h"
-int Register :: counter = 0 ;
 Register :: Register() {
     Register_Container[0] = 0 ;
 }
 Register ::Register(long address, long value) {
     Register_Container[address] = value ;
 }
-void Register :: Set_Value(long value) {
-  Register_Container[counter] = value ;
+void Register :: Set_Value(long address ,long value) {
+  Register_Container[address] = value ;
 }
-long Register :: Get_Value() {
-    return Register_Container[counter] ;
+long Register :: Get_Value(long address) {
+    return Register_Container[address] ;
 }
