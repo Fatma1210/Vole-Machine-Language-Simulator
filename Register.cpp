b8ce3b0bfd2c57300f1b6f81c21dfbb87992ee32
Fatma İbrahim <113267262+Fatma1210@users.noreply.github.com>
2023-11-12 10:59:32 +0200

    Add files via upload

---
+++ Register.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std ;
+#include "Register.h"
+int Register :: counter = 0 ;
+Register :: Register() {
+    Register_Container[0] = 0 ;
+}
+Register ::Register(long address, long value) {
+    Register_Container[address] = value ;
+}
+void Register :: Set_Value(long value) {
+  Register_Container[counter] = value ;
+}
+long Register :: Get_Value() {
+    return Register_Container[counter] ;
+}
---
+++ Register.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <map>
+using namespace std ;
+#ifndef VOLEMACHINELANGUAGESIMULATOR_REGISTER_H
+#define VOLEMACHINELANGUAGESIMULATOR_REGISTER_H
+    class Register {
+    private:
+        static int counter ;
+        map < long , long > Register_Container ;
+    public:
+        Register() ;
+        Register(long address , long value) ;
+        void Set_Value (long val) ;
+        long Get_Value() ;
+    };
+
+
+#endif //VOLEMACHINELANGUAGESIMULATOR_REGISTER_H
