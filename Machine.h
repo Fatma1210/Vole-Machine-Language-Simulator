84ce3a31fb085e060f2bd876929471af02b6fbde
Maya Fouad <112335115+mayafouad@users.noreply.github.com>
2023-11-15 22:44:10 +0200

    Update Machine.h

--- Machine.h
+++ Machine.h
@@ -10,18 +10,20 @@ private:
     long op;
     long r;
     long m;
-
+    long step;
 public:
     Register R;
     Memory M;
     Machine();
     void open(string);
+    void work();
     long convert(string);
     void operations(long, long, long );
     long get_counter();
     void Load(long address_of_R , long address_of_XY ) ;
     void Move(long address_of_R , long address_of_S) ;
     void JUMP(long address_of_R , long address_of_XY ) ;
+    long Screen ();
 };
 
 #endif //MACHINE_H
