f534c29262d0a2fadd80094ae174ccd36468dbdf
Fatma İbrahim <113267262+Fatma1210@users.noreply.github.com>
2023-11-15 17:27:24 +0200

    Update Memory.cpp

--- Memory.cpp
+++ Memory.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
 #include "Memory.h"
 using namespace std ;
-
 Memory::Memory(int size) : data(size, 0) {}
 
-Memory::long read(long address){
-  if (address >= 0 && address < data.size()) {
-      return data[address];
-  } else {
-      cerr << "Memory read error: Invalid address " << address << endl;
-      return 0;
-  }
+long Memory:: read(long address){
+    if (address >= 0 && address < data.size()) {
+        return data[address];
+    } else {
+        cerr << "Memory read error: Invalid address " << address << endl;
+        return 0;
+    }
 }
-Memory::void write (long address, long value) {
-  if (address >= 0 && address < data.size()) {
-      data[address] = value;
-  } else {
-      cerr << "Memory write error: Invalid address " << address << endl;
-  }
+void Memory :: write (long address, long value) {
+    if (address >= 0 && address < data.size()) {
+        data[address] = value;
+    } else {
+        cerr << "Memory write error: Invalid address " << address << endl;
+    }
 }
